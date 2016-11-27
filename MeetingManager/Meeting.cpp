188a76b605889426c9c68cb27be71d16056cfc92
김성연 <@DESKTOP-FKQDG7E>
2016-11-27 21:34:19 +0900

    Meeting update by sungyeon

--- MeetingManager/Meeting.cpp
+++ MeetingManager/Meeting.cpp
@@ -2,59 +2,101 @@
 #include "Meeting.h"
 
 
-Meeting::Meeting(string topic_, string day_, double startTime_, double endTime_)
+Meeting::Meeting(string day_, double startTime_, double endTime_, string topic_)
 {
-	topic = topic_;
-	day = day_;
-	startTime = startTime_;
-	endTime = endTime_;
+	m_topic = topic_;
+	m_day = day_;
+	m_startTime = startTime_;
+	m_endTime = endTime_;
 }
 
-void Meeting::insrtParticipation(unordered_map<string, auto_ptr<Person>>& People, string name) {
-	if (People.find(name) != People.end())	// 이름이 name인 Person이 존재하면
+/*Participation안에 이름이 name인 person이 존재하는지를 검사한다.
+*/
+bool Meeting::isPerson(string name)
+{
+	for (auto it = m_Participation.begin(); it != m_Participation.end(); ++it) {
+		if (name != it->second.getName())
+			return true;
+	}
+	return false;
+}
+
+void Meeting::addParticipation(unordered_map<string, Person>& People, string name) {
+	
+	if (People.find(name) != People.end()) // People 안에 이름이 name인 Person이 존재하면(추가할 수 있다)
 	{
-		Participation[name] = People.find(name)->second;
+		if (isPerson(name))	// Participation 안에 이름이 name인 Person이 이미 존재하면
+		{
+			cerr << "Participation 안에 "<< "이름이 " << name << " 인 Person이 이미 존재합니다." << endl;
+
+		}
+		else {
+			m_Participation.emplace(name, People.find(name)->second);
+		}
 	}
 	else {
-		cout << "이름이 " << name << " 인 Person이 존재 하지 않습니다." << endl;
+		cerr << "People 안에 " << "이름이 " << name << " 인 Person이 존재 하지 않습니다." << endl;
 	}
+}
+
+/*Participation을 삭제한다.
+해당 Participation이 존재하는지 확인한다.
+*/
+void Meeting::delParticiaption(string name)
+{
+	//Person 존재 확인
+	if(isPerson(name))
+		cerr << "이름이 " << name << " 인 Person이 존재 하지 않습니다." << endl;
+	//Person 삭제
+	else {
+		m_Participation.erase(name);
+	}
+}
+
+/*해당하는 Participation을 반환한다.
+해당 Participation이 존재하는지 확인한다.
+*/
+Person& Meeting::getParticipation(string name)
+{
+	//Person 존재 확인
+	if (isPerson(name))
+		cerr << "이름이 " << name << " 인 Person이 존재 하지 않습니다." << endl;
+	//Person 반환
+	else
+		return m_Participation[name];
+	
 }
 
 string Meeting::getTopic() const {
-	return topic;
+	return m_topic;
 }
 
 string Meeting::getDay() const {
-	return day;
+	return m_day;
 }
 
-double Meeting::getsStartTime() const {
-	return startTime;
+double Meeting::getStartTime() const {
+	return m_startTime;
 }
 
 double Meeting::getEndTime() const {
-	return endTime;
+	return m_endTime;
 }
-
-auto_ptr<Person> Meeting::getParticipation(string name){
-	return Participation[name];
-}
-
 
 void Meeting::setTopic(const string new_topic) {
-	topic = new_topic;
+	m_topic = new_topic;
 }
 
 void Meeting::setDay(const string new_day) {
-	day = new_day;
+	m_day = new_day;
 }
 
 void Meeting::setStartTime(const double new_startTime) {
-	startTime = new_startTime;
+	m_startTime = new_startTime;
 }
 
 void Meeting::setEndTime(const double new_endTime) {
-	endTime = new_endTime;
+	m_endTime = new_endTime;
 }
 
 Meeting::~Meeting()
--- MeetingManager/Meeting.h
+++ MeetingManager/Meeting.h
@@ -1,19 +1,32 @@
 #pragma once
+#include<iostream>
+#include<unordered_map>
 #include<string>
+#include "Person.h"
+using namespace std;
 /*성연 code와 호환성 검사용
 */
 using namespace std;
 class Meeting
 {
 private:
-	string m_meetingId;
+	unordered_map<string, Person> m_Participation; //container for Person
+	string m_topic;
 	string m_day;
 	double m_startTime;
 	double m_endTime;
 public: //디버깅용
-	Meeting(string day, double startTime, double endTime, string topic, string meetingId) { };
-	string getMeetingId() const { return m_meetingId; }
-	string getDay() const { return m_day; }
-	double getStartTime() const { return m_startTime; }
-	double getEndTime() const { return m_endTime; }
+	Meeting(string day_, double startTime_, double endTime_, string topic_);
+	bool Meeting::isPerson(string name);
+	void addParticipation(unordered_map<string, Person>& People, string name);
+	Person& getParticipation(string name);
+	void delParticiaption(string name);
+	string getTopic() const;
+	string getDay() const;
+	double getStartTime() const;
+	double getEndTime() const;
+	void setTopic(const string new_topic);
+	void setDay(const string new_day);
+	void setStartTime(const double new_startTime);
+	void setEndTime(const double new_endTime);
 };
